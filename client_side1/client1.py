90ed243575616dad6ef1ce3cd4ad875752b97eab
unknown <hungluu111103@gmail.com>
2023-11-15 14:00:01 +0700

    first version

---
+++ client_side1/client1.py
@@ -0,0 +1,135 @@
+import socket
+import os
+import threading
+
+# Client Configuration
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 12345
+
+CLIENT_SERVER_IP = "127.0.0.1"
+CLIENT_SERVER_PORT = 12346
+
+CLIENT_ID = "127.0.0.1:12346"
+
+# Function to send requests to the server
+def send_request(request, client_socket):
+    client_socket.send(request.encode())
+    response = client_socket.recv(1024).decode()
+    print("Server response:", response)
+    return response
+
+def handle_incoming_request(client_socket):
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data:
+            break
+        # Process and handle the request from the other client
+        parts = data.split()
+        if parts[0] == "FETCH":
+            # Handle FETCH request here
+            file_path = "client_repository/" + parts[1]
+            send_file(client_socket, file_path)
+            pass
+        if parts[0] == "PING":
+            # Handle FETCH request here
+            client_socket.send("PONG".encode())
+            pass
+        # Add more handlers for other request types
+
+    client_socket.close()
+
+def listen_for_server():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((SERVER_IP, SERVER_PORT))
+    while True:
+        command = input("Enter a command (publish, fetch): ").strip().split()
+        if command[0] == "publish":
+            # Implement the logic to publish a file here
+            publish_file(CLIENT_ID, command[1], command[2], client_socket)
+            pass
+        elif command[0] == "fetch":
+            # Fetch client has file
+            target_clients = fetch_file_locations(command[1], client_socket)
+            fetch_handler = threading.Thread(target=fetch_and_receive_file, args=(command[1], target_clients))
+            fetch_handler.start()
+            pass
+        else:
+            print("Invalid command. Supported commands: CONNECT, PUBLISH, FETCH")
+
+def send_file(client_socket, file_path):
+    with open(file_path, 'rb') as file:
+        data = file.read(1024)  # Read 1 KB at a time (adjust as needed)
+        while data:
+            client_socket.send(data)
+            data = file.read(1024)
+
+def fetch_and_receive_file(file_name, client_id):
+    ip_address, port = client_id.split(':')
+    port = int(port)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip_address, port))
+    
+    fetch_request = f"FETCH {file_name}"
+    client_socket.send(fetch_request.encode())
+
+    repository_path = "client_repository/"
+    repository_path = os.path.join(repository_path, file_name)
+    os.makedirs(os.path.dirname(repository_path), exist_ok=True)
+    with open(repository_path, 'wb') as file:
+        while True:
+            data = client_socket.recv(1024)  # Receive 1 KB at a time (adjust as needed)
+            if not data:
+                break
+            file.write(data)
+
+    inform_fetched_file(CLIENT_ID, file_name)
+    print("done")
+
+# Function to publish a file
+def publish_file(client_id, lname, fname, client_socket):
+    # Check if the file exists in the specified local path (lname)
+    if os.path.exists(lname):
+        # Copy the file to the client's repository (you need to define a path for the repository)
+        repository_path = "client_repository/"
+        os.makedirs(repository_path, exist_ok=True)
+        target_file_path = os.path.join(repository_path, fname)
+        with open(lname, "rb") as source_file, open(target_file_path, "wb") as target_file:
+            target_file.write(source_file.read())
+        
+        # Send a "PUBLISH" request to inform the server
+        request = f"PUBLISH {client_id} {fname}"
+        send_request(request, client_socket)
+    else:
+        print("The specified file does not exist in the local path.")
+
+# Function to fetch target clients for a file
+def fetch_file_locations(file_name, client_socket):
+    request = f"FETCHCLIENT {file_name}"
+    return send_request(request, client_socket)
+
+# Function to fetch a file
+def fetch_file(file_name):
+    request = f"FETCHFILE {file_name}"
+    send_request(request)
+
+# Function to inform the server about a fetched file
+def inform_fetched_file(client_id, file_name):
+    request = f"INFORM {client_id} {file_name}"
+    send_request(request)
+
+def main():
+    server_listener = threading.Thread(target=listen_for_server)
+    server_listener.start() 
+    request_handling_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    request_handling_socket.bind((CLIENT_SERVER_IP, CLIENT_SERVER_PORT))  # Change the port as needed
+    request_handling_socket.listen(5)
+    print("Client server listening on 127.0.0.1:12346")
+    while True:
+        client_socket, addr = request_handling_socket.accept()
+        print(f"Accepted connection from {addr[0]}:{addr[1]}")
+        request_handler = threading.Thread(target=handle_incoming_request, args=(client_socket,))
+        request_handler.start()
+
+# Start the main thread to listen for incoming connections from other clients
+if __name__ == "__main__":
+    main()
---
+++ server_side/server.py
@@ -0,0 +1,101 @@
+import socket
+import threading
+
+# Data structures to store information about published files and clients
+published_files = {}
+clients = {}
+
+# Server Configuration
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 12345
+
+# Function to handle client requests
+def handle_client(client_socket):
+    while True:
+        data = client_socket.recv(1024).decode()
+        if not data:
+            break
+        parts = data.split()
+        if parts[0] == "PUBLISH":
+            handle_publish_file(client_socket, parts[1], parts[2])
+        elif parts[0] == "FETCHCLIENT":
+            handle_fetch_file_location(client_socket, parts[1])
+        elif parts[0] == "INFORM":
+            handle_inform_fetched_file(client_socket, parts[1], parts[2])
+        else
+            print("Invalid command")
+
+# Function to publish a file to the server
+def handle_publish_file(client_socket, client_id, file_name):
+    # Store file information in the published_files data structure
+    if file_name in published_files:
+        # If the file is already in the dictionary, append the client_id to the existing list
+        published_files[file_name].append(client_id)
+    else:
+        # If the file is not in the dictionary, create a new list with the client_id
+        published_files[file_name] = [client_id]
+    print(client_id)
+    response = "File published successfully"
+    client_socket.send(response.encode())
+
+# Function to fetch target clients that have a file
+def handle_fetch_file_location(client_socket, file_name):
+    target_clients = published_files.get(file_name, [])
+    response = " ".join(target_clients)
+    client_socket.send(response.encode())
+
+# Function to fetch a file from a target client
+
+# Function to inform the server about a fetched file
+def handle_inform_fetched_file(client_socket, client_id, file_name):
+    # You can update data structures or perform any necessary actions
+    published_files[file_name].append(client_id)
+    response = "File information updated"
+    client_socket.send(response.encode())
+
+# Function to discover file names of a client
+def discover_file_names(client_id):
+    client_files = []
+    for file, clients in published_files.items():
+        for client in clients:
+            if client == client_id: client_files.append(file)
+    print(published_files)
+    response = " ".join(client_files)
+    print(response)
+
+# Function to ping a host
+def ping_host(client_id):
+    ip_address, port = client_id.split(':')
+    port = int(port)
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ip_address, port))
+    request = "PING " + client_id
+    client_socket.send(request.encode())
+    response = client_socket.recv(1024).decode()
+    print(response)
+
+def handle_command():
+    while True:
+        command = input("Enter a command for the server (ping, discover): ").strip().split()
+        if command[0] == "ping":
+            ping_host(command[1])
+            pass
+        elif command[0] == "discover":
+            discover_file_names(command[1])
+            pass
+        else:
+            print("Invalid command")
+
+# Server setup
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((SERVER_IP, SERVER_PORT))
+server.listen(5)
+print(f"Server listening on {SERVER_IP}:{SERVER_PORT}")
+command_handler = threading.Thread(target=handle_command)
+command_handler.start()
+
+while True:
+    client_socket, addr = server.accept()
+    print(f"Accepted connection from {addr[0]}:{addr[1]}")
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))
+    client_handler.start()
